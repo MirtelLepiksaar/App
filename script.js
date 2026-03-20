d45fcdafd500de905017a2de78ffa06e3084145c
MirtelLepiksaar <mirtel.lepiksaar@techno.ee>
2026-03-20 14:04:01 +0200

    mingi värk siin on ja töötab kah

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-
-async function mdea() {
-    const a = { mina: "perses" };
-const response = await fetch('https://tinkr.tech/sdb/MirteliDB', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify(a)
+const nupp = document.querySelector("button");
+nupp.addEventListener("click", function () {
+  async function Jobud() {
+    const response = await fetch('https://tinkr.tech/sdb/Mirtel-Lepiksaar/Tiny-Chat', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: 'John', age: 25 })
+    });
+  };
+  Jobud();
+  
 });
-const data = response.json()
-console.log(data)
-}
