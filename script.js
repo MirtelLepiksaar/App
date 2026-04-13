a1ffe89f6ba706f5367bca244426cf0ed98615cc
unknown <mirtel.lepiksaar@techno.ee>
2026-04-13 22:50:41 +0300

    almost redi

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const nupp = document.querySelector("button");
 const text_bar = document.querySelector("input");
+const div = document.querySelector("div");
+const body = document.querySelector("body");
+const username = document.createElement("input").readonly;
+username.classList.add("username")
+body.appendChild(username)
 nupp.addEventListener("click", function () {
   async function saada() {
     const response = await fetch('https://tinkr.tech/sdb/Mirtel-Lepiksaar/Tiny-Chat', {
@@ -9,10 +14,11 @@ nupp.addEventListener("click", function () {
     });
   };
   saada();
+  
+  const minaraagin = document.createElement("strong");
+  div.appendChild(minaraagin);
+  minaraagin.classList.add("mina");
+  minaraagin.textContent = text_bar.value;
   text_bar.value = "";
-  let minaräägin = document.createElement("p");
-  async function võta() {
-    
-  }
-  minaräägin.textContent = 
+  
 });
